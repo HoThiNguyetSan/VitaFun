35a287bfbf4c7f82a837cd22bdf205077f5fcaab
VoCongTao12345 <92636739+VoCongTao12345@users.noreply.github.com>
2021-12-23 14:19:24 +0700

    udate2h

--- javascript/GioHang.js
+++ javascript/GioHang.js
@@ -1,52 +1,30 @@
-// function Gia1(){
-//     var a=document.getElementById("item1").value;
-//     a = parseFloat(a);
-//     var kq= 99000*a;
-//     document.getElementById("kq1").value = kq;
-//     document.getElementById("checkbox1").value = kq;
-// }
 
-// function Gia2(){
-//     var a=document.getElementById("item2").value;
-//     a = parseFloat(a);
-//     var kq= 40000*a;
-//     document.getElementById("kq2").value = kq;
-//     document.getElementById("checkbox2").value = kq;
-// } 
-
-// document.getElementById('btn').onclick = function()
-// {
-//     var checkbox = document.getElementsByName('chon');
-//     var result = 0;    
-    
-//     for (var i = 0; i < checkbox.length; i++){
-//         if (checkbox[i].checked === true){           
-//             var a = parseFloat(checkbox[i].value);
-//             result += a ;
-//         }
-//     }      
-//     document.getElementById('Total').innerHTML=result+'đ'      
-    
-// };
-let app = angular.module("BestsellerApp",[])
-app.controller("BestsellerProduct",function($scope,$http){
+let app = angular.module("CartApp",[])
+app.controller("CartrProduct",function($scope,$http){
     $http({
         method:"GET",
         url:"../Data/ProductsBuyed.json"
     })
     .then(
         function success(response){
-            $scope.ProductBuyed = response.data
-            
+            $scope.ProductBuyed = response.data     
+                 
         },
         function error(response){
             $scope.error = response.statusText
         }
     );   
-   $scope.Tong=0
-   $scope.TongAll= function(){
-       return Tong+2
-   }
+    $scope.getAmount = function(){
+        var amount = 0;
+        $scope.ProductBuyed.forEach(element => {
+            if(element.buy){
+                amount+=element.Gia*element.Soluong
+            }
+            
+        });
+        return amount
+    }
+    
 })
 
 
--- javascript/ThanhToan.js
+++ javascript/ThanhToan.js
@@ -1,39 +1,27 @@
-var txtQuantity=Array.from(document.querySelectorAll('.txtQuanti')),
-    price=Array.from(document.querySelectorAll('.payment-product__price')),
-    money=Array.from(document.querySelectorAll('.payment-product__money')),
-    totalOrder=document.getElementById('totalOrder'),
-    feeDelive=document.getElementById('feeDelive'),
-    totalPayment=document.getElementById('totalPayment')
-  
 
-for(var i=0;i<txtQuantity.length;i++){
-    txtQuantity[i].addEventListener('change',function(e){        
-        var c=e.target.id
-        console.log(c)
-        money[c].innerHTML=parseFloat(price[c].innerHTML)*parseFloat(txtQuantity[c].value)+'đ'
-        var tong=0;
-
-        for(var z=0;z<txtQuantity.length;z++){
-            
-            tong+=parseFloat(money[z].innerHTML)
-        }
-        console.log(tong)
-        totalOrder.innerHTML=tong+'đ';
-
-        if(parseFloat(totalOrder.innerHTML)>500000){
-            feeDelive.innerHTML='0đ'
-        }
-        else if(parseFloat(totalOrder.innerHTML)>250000){
-            feeDelive.innerHTML='5000đ'
-        }
-        else{
-            feeDelive.innerHTML='10000đ'
-        }
-
-        totalPayment.innerHTML=parseFloat(totalOrder.innerHTML)+parseFloat(feeDelive.innerHTML)
-        
+let app = angular.module("PaymentApp",[])
+app.controller("PaymentProduct",function($scope,$http){
+    $http({
+        method:"GET",
+        url:"../Data/ProductsBuyed.json"
     })
-}
+    .then(
+        function success(response){
+            $scope.ProductPayment = response.data
+            
+        },
+        function error(response){
+            $scope.error = response.statusText
+        }
+    );   
+    $scope.getAmount = function(){
+        var amount = 0;
+        $scope.ProductPayment.forEach(element => {           
+            amount+=element.Gia*element.Soluong             
+    });
+        return amount
+    }
+})
 
 
 
